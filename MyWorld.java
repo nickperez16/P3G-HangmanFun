e8ed8c55a0d34a8390fc4badea1f9b09fbbd0a26
335640447 <93529143+335640447@users.noreply.github.com>
2021-11-05 11:05:15 -0400

    Add files via upload

--- MyWorld.java
+++ MyWorld.java
@@ -1,107 +1,38 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.net.*;
+import java.io.*;
 import java.util.ArrayList;
-import java.util.*;
 
-/**
- * The main world.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class MyWorld extends World
 {
-    private int counter;
-    
     /**
-     * Constructor for objects of class MyWorld.
-     * 
+     * Apparently now it doesn't work, but the 'act' method
+     * should still work as it doesn't use "Letter"
      */
+    
+    char[] letters = new Letter[26];
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-
-
-        super(800, 600, 1); 
-        prepBase();
-        prepHead();
-        prepBody();
-        prepRArm();
-        prepLArm();
-        prepRLeg();
-        prepLLeg();
-        ArrayList<String> list = new ArrayList<String>();
-        try {
-            Reader.readInto(list);
-        } catch(Exception e) {
-
-
+        super(600, 400, 1); 
+        for(int i = 0; i < 26; i++) 
+        {
+            addObject(letters[i] = new Letter(i),
+            25 + 38 * (i % 13), 510 + 30 * (i / 13));
         }
-
-    
-
-      
-        //The following code picks a random word from the list of words, and finds the length of the word    
-        int n = Greenfoot.getRandomNumber(1000);
-        String s = (list.get(n));
-        System.out.println(s);
-        String strLength = s.length() + "";
-        
-
-        //gives players a hint on word.
-        showText("the word has " + strLength + " letters",400,550); 
-        
-        
     }
 
-    private void prepBase()
+     private void letterPressed(String keyInput)
     {
-        HmBase hangmanBase = new HmBase();
-        addObject(hangmanBase,300,250);
+        letterInput(letters[(int)keyInput.charAt(0)-65]);
     }
-
-    private void prepHead()
+    public void act()
     {
-        HmHead hangmanHead = new HmHead();
-        addObject(hangmanHead,200,152);
-   
-    }   
-
-    //prepare body for hangman
-    private void prepBody()
-    {
-        HmBody hangmanBody = new HmBody();
-        addObject (hangmanBody,200,152);
-    
-    }
-
-    //prepare right arm
-    private void prepRArm()
-    {
-        RightArm hangmanRArm = new RightArm(); 
-        addObject (hangmanRArm,200,152);
-    }
-
-    //prepare left arm
-    private void prepLArm()
-    {
-        LeftArm hangmanLArm = new LeftArm();
-        addObject(hangmanLArm,200,152);
-    }
-
-    //prepare right leg
-
-    private void prepRLeg()
-    {
-        RightLeg hangmanRLeg = new RightLeg();
-        addObject(hangmanRLeg,200,152);
-    }
-
-    //prepare left leg
-
-    private void prepLLeg()
-    {
-        LeftLeg hangmanLLeg = new LeftLeg();
-        addObject(hangmanLLeg,200,152);
+        String keyInput = Greenfoot.getKey();
+        if(keyInput != null && keyInput.length() == 1)
+        {
+            keyInput = keyInput.toUpperCase();
+            if(keyInput.charAt(0) >= 'A' && keyInput.charAt(0) <= 'Z') letterPressed(keyInput);
+        }
     }
 }
-
